388da0945ea48df32c0ab041cef01cb01cb28da5
Matthew Evans <mevans90@gmail.com>
2022-09-13 10:16:00 -0400

    complete section 34

---
+++ Section-34/firstEJSApp/index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, '/views'))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/cats', (req, res) => {
+    const cats = ['Blue', 'Rocket', 'Stephanie', 'Winston']
+    res.render('cats', { cats })
+})
+
+app.get('/rand', (req, res) => {
+    const num = Math.floor(Math.random() * 10) + 1
+    res.render('random', { rand: num })
+})
+
+app.get('/r/:subreddit', (req, res) => {
+    const { subreddit } = req.params
+    res.render('subreddit', { subreddit })
+})
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000!')
+})
